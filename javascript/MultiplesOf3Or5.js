87f38a7aa7594483126c0ee0517c6319a01f8444
aiyu666 <zrobin8741@gmail.com>
2020-07-11 11:42:29 +0800

    Complete MultiplesOf3Or5

---
+++ javascript/MultiplesOf3Or5.js
@@ -0,0 +1,19 @@
+/** Multiples of 3 or 5
+ * [6 kyu]
+ *
+ * If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+ *
+ * Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in.
+ *
+ * From: https://www.codewars.com/kata/514b92a657cdc65150000006/train/javascript
+ */
+
+function solution(number) {
+    let sum = 0
+    for (let num = 3; num < number; num++) {
+        if (num % 3 === 0 || num % 5 === 0) sum += num;
+    }
+    return sum
+}
+
+console.log(solution(10))  //23
