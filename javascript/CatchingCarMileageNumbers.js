b630bdf0bdc74a061cf22e94e9599864517a8e7d
aiyu666 <zrobin8741@gmail.com>
2020-07-16 14:06:46 +0800

    Update best practice

--- javascript/CatchingCarMileageNumbers.js
+++ javascript/CatchingCarMileageNumbers.js
@@ -44,6 +44,27 @@
  * You should only ever output 0, 1, or 2.
  *
  * from: https://www.codewars.com/kata/52c4dd683bfd3b434c000292/train/javascript
+ * 
+ * Best practice: 
+ * function isInteresting(number, awesomePhrases) {
+ * var tests = [
+ *   function(n) { return /^\d00+$/.test(n); },
+ *   function(n) { return /^(\d)\1+$/.test(n); },
+ *   function(n) { return RegExp(n).test(1234567890); },
+ *   function(n) { return RegExp(n).test(9876543210); },
+ *   function(n) { return n + '' == (n + '').split('').reverse().join(''); },
+ *   function(n) { return awesomePhrases.some(function(p) { return p == n; }); }
+ * ];
+ * 
+ * var interesting = 0;
+ * tests.some(function(test) {
+ *   if (number > 99 && test(number))
+ *     return interesting = 2;
+ *   else if ((number > 98 && test(number + 1)) || (number > 97 && test(number + 2)))
+ *     interesting = 1;
+ * });
+ * return interesting;
+ * } 
 */
 
 function isInteresting(number, awesomePhrases) {
