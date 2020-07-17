2ebe8597c1ff8aa6f9e2713a2be9a8978888ecf7
aiyu666 <zrobin8741@gmail.com>
2020-07-17 13:30:34 +0800

    Stage RecoverASecretStringFromRandomTriplets

--- javascript/RecoverASecretStringFromRandomTriplets.js
+++ javascript/RecoverASecretStringFromRandomTriplets.js
@@ -12,8 +12,51 @@
  * from: https://www.codewars.com/kata/53f40dff5f9d31b813000774/train/javascript
  */
 
-var recoverSecret = function(triplets) {
+function searchAndApeend() {
+
 }
+
+var recoverSecret = function(triplets) {
+    let result = []
+    let checkList = []
+    // list all word
+    triplets.map(element => {
+        for (let i = 0; i < element.length; i++) {
+            if (checkList.indexOf(element[i]) === -1) checkList.push(element[i])
+        }
+    })
+    console.log(checkList)
+
+    // find first word
+    const firstValue = checkList.filter(val => {
+        let count = 0
+        for (let i = 0; i < triplets.length; i++) {
+            if (triplets[i][1] === val || triplets[i][2] === val) count++
+        }
+        if (count === 0) return val
+    }).join('')
+    result.push(firstValue)
+    let preValue = firstValue
+    for (let index = 0; index < checkList.length - 1; index++) {
+        preValue = searchElement(preValue, triplets)
+        result.push(preValue)
+    }
+    return result.join('')
+}
+function searchElement(preNum, triplets) {
+    console.log(`preNum ${preNum}`)
+    const res = triplets.filter(element => {
+        return (element[0] === preNum)
+    });
+    if (res.length !== 0) return res[0][1]
+    const resp = triplets.filter(element => {
+        return (element[1] === preNum)
+    });
+    if (resp.length !== 0) return resp[0][2]
+    return
+}
+
+// "whatisup"
 const triplets1 = [
     ['t', 'u', 'p'],
     ['w', 'h', 'i'],
@@ -23,4 +66,29 @@ const triplets1 = [
     ['t', 'i', 's'],
     ['w', 'h', 's']
 ]
+
+
 console.log(recoverSecret(triplets1)) // "whatisup"
+
+//mathisfun
+
+// const triplets2 = [
+//     ['t', 's', 'f'],
+//     ['a', 's', 'u'],
+//     ['m', 'a', 'f'],
+//     ['a', 'i', 'n'],
+//     ['s', 'u', 'n'],
+//     ['m', 'f', 'u'],
+//     ['a', 't', 'h'],
+//     ['t', 'h', 'i'],
+//     ['h', 'i', 'f'],
+//     ['m', 'h', 'f'],
+//     ['a', 'u', 'n'],
+//     ['m', 'a', 't'],
+//     ['f', 'u', 'n'],
+//     ['h', 's', 'n'],
+//     ['a', 'i', 's'],
+//     ['m', 's', 'n'],
+//     ['m', 's', 'u']]
+
+// console.log(recoverSecret(triplets2)) // "mathisfun"
