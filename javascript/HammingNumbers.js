fece2e3dcd619d0de56210ea46c5d9e60c8d1c73
aiyu666 <zrobin8741@gmail.com>
2020-07-23 14:05:53 +0800

    Pending Haming and add from url

--- javascript/HammingNumbers.js
+++ javascript/HammingNumbers.js
@@ -14,21 +14,28 @@
  * The 20 smallest Hamming numbers are given in example test fixture.
  *
  * Your code should be able to compute all of the smallest 5,000 (Clojure: 2000, NASM: 13282) Hamming numbers without timing out.
- *
+ * from: https://www.codewars.com/kata/526d84b98f428f14a60008da/train/javascript
  */
 
 function hamming(n) {
-
+    let result = [1];
+    for (let i = 1; i <= 1000; i += 1) {
+        if (i % 2 === 0) result.push(i)
+        else if (i % 3 === 0) result.push(i)
+        else if (i % 5 === 0) result.push(i)
+    }
+    console.log(result);
+    return result[n - 1];
 }
-
-// console.log(hamming(1) == 1,);
-// console.log(hamming(2) == 2,);
-// console.log(hamming(3) == 3,);
-// console.log(hamming(4) == 4,);
-// console.log(hamming(5) == 5,);
-// console.log(hamming(6) == 6,);
-// console.log(hamming(7) == 8,);
-console.log(hamming(8) == 9,);
+// 3 6 9 12 15 18 21 24 27 30 33 36 39
+console.log(hamming(1) == 1);
+console.log(hamming(2) == 2);
+console.log(hamming(3) == 3);
+console.log(hamming(4) == 4);
+console.log(hamming(5) == 5);
+console.log(hamming(6) == 6);
+// console.log(hamming(7) == 8);
+// console.log(hamming(8) == 9);
 // console.log(hamming(9) == 10);
 // console.log(hamming(10) == 12);
 // console.log(hamming(11) == 15);
