d2a52b1be2a34cf1bb0c81f3b47eb787d4044be9
aiyu666 <zrobin8741@gmail.com>
2020-07-30 13:33:11 +0800

    Complete WeatherObservationStation1

---
+++ SQL/WeatherObservationStation1.sql
@@ -0,0 +1,11 @@
+-- Weather Observation Station 1
+-- Query a list of CITY and STATE from the STATION table.
+-- The STATION table is described as
+-- follows:
+
+-- where LAT_N is the northern latitude and LONG_W is the western longitude.
+
+-- from: https://www.hackerrank.com/challenges/weather-observation-station-1/problem?h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+
+SELECT CITY, STATE
+FROM STATION ;
