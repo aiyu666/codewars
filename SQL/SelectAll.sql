3cdf90449180485199cf8f80ad4f4f87af246759
aiyu666 <zrobin8741@gmail.com>
2020-07-30 13:19:25 +0800

    Complete SelectAll

---
+++ SQL/SelectAll.sql
@@ -0,0 +1,10 @@
+-- Query
+-- all columns
+-- (attributes) for every row in the CITY table.
+
+-- The CITY table is described as
+-- follows:
+-- from: https://www.hackerrank.com/challenges/select-all-sql/problem?h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+
+SELECT *
+FROM CITY;
