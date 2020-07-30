f90dfeaf360f30e5e3a996d1b7ca13b42ceaac01
aiyu666 <zrobin8741@gmail.com>
2020-07-30 13:21:22 +0800

    Complete SelectByID

---
+++ SQL/SelectByID.sql
@@ -0,0 +1,8 @@
+-- Query all columns for a city in CITY with the ID 1661.
+
+-- The CITY table is described as follows:
+-- from: https://www.hackerrank.com/challenges/select-by-id/problem?h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+
+SELECT *
+FROM CITY
+WHERE ID ='1661';
